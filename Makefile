65988fdcd6b092bedc06ed3a6ff3dc3dcb229c0b
Lorcan McDonald <lorcan@lorcanmcdonald.com>
2017-06-11 17:07:27 +0100

    Improve build process

---
+++ Makefile
@@ -0,0 +1,37 @@
+
+all: javascript html style images fonts
+	
+javascript: bin/index.js
+
+html: bin/index.html
+
+style: bin/style.css
+
+images: bin/images
+
+fonts : bin/font
+
+bin/images: bin $(wildcard src/images/*)
+	mkdir -p bin/images
+	cp $(wildcard src/images/*) bin/images
+
+bin/font/: bin $(wildcard src/font/*)
+	mkdir -p bin/font
+	cp $(wildcard src/font/*) bin/font
+
+bin/index.js : bin src/main.elm
+	./node_modules/.bin/elm make --output bin/index.js src/main.elm
+
+bin/index.html : bin src/index.html
+	cp src/index.html bin/
+
+bin/style.css : bin src/style.css
+	cp src/style.css bin/
+
+bin :
+	mkdir bin
+
+.PHONY: clean
+clean :
+	-rm -r bin
+
--- src/main.elm
+++ src/main.elm
@@ -1,4 +1,3 @@
-
 import Html exposing (..)
 import Html.Attributes exposing (id, style, class, href, src, target)
 import Html.Events exposing (onClick)
@@ -32,13 +31,16 @@ home = div [ class "content"]
            , title
            , introduction
            , projects
+           , copyright
            , work
+           , copyright
+           , break
            , education
            , copyright
            ]
 
 copyright : Html
-copyright = article [class "small-print"] [text "This document is covered by copyright, modifications and derived works forbidden"]
+copyright = article [class "small-print"] [Markdown.toHtml "**This document is covered by copyright. Modifications and derived works forbidden.**"]
 
 menu : Html
 menu = nav []
@@ -54,11 +56,12 @@ menu = nav []
 introduction : Html
 introduction = section [] [
     article [] [ Markdown.toHtml """
-I am an experienced and highly capable Principal Software Engineer. I have a passion for
-identifying ways to improve efficiency in software development, especially through
-automation: if a computer can do it a computer should do it.
+I have a passion for identifying ways to improve efficiency in software
+development, especially through automation: if a computer can do it a computer
+should do it. This allows my teams to spend more time on producing insights
+and innovation at scale.
 
-♥︎ Haskell, Javascript/NodeJS, Docker, the UNIX command line and ♡♥︎Vim
+♥︎ Haskell, the UNIX command line, Javascript, Docker, and ♡♥︎Vim.
 """ ] ]
 
 menuLink : Url -> String -> Html
@@ -73,7 +76,6 @@ menuLink url content = case url of
 banner : Html
 banner = header []
                 [ div [] [ text "_" ]
-
                 ]
 title : Html
 title = h1 [] [ em [] [ text "Lorcan"]
@@ -83,8 +85,18 @@ title = h1 [] [ em [] [ text "Lorcan"]
 projects : Html
 projects = section [ id "Projects" ]
                  [ h2 [] [ text "Projects" ]
+                 , github "regexicon.com"
+                     (ExternalUrl "https://github.com/lorcanmcdonald/RegexCandidates")
+                     (Just (LocalUrl "images/regexicon.com.png"))
+                     (Markdown.toHtml """A common problem I've noticed when code reviewing regular expressions is
+not that they don't match intended pattern, but rather that they will match
+something unexpected. I wrote this web service to help identify these issues by
+showing a selection of strings that would match a given regex.
+
+You can try it out at [http://regexicon.com/](http://regexicon.com/)
+""" )
                  , github "mars" (toUrl "https://github.com/lorcanmcdonald/mars")
-                     (Just (LocalUrl "mars-static.png"))
+                     (Just (LocalUrl "images/mars-static.png"))
                      (Markdown.toHtml """Mars is a REPL for exploring JSON
 documents. It allows you to use familiar UNIX shell
 commands to explore and update. I've found it useful to
@@ -93,25 +105,6 @@ get an overview of an unfamiliar API.
 It's written in Haskell using [Parsec](https://hackage.haskell.org/package/parsec) to parse the
 command line mini language.
 """ )
-                 , github "RegexCandidates"
-                     (ExternalUrl "https://github.com/lorcanmcdonald/RegexCandidates")
-                     Nothing
-                     (Markdown.toHtml """A common problem I've noticed when reviewing regular expressions is
-not that they don't match intended pattern, but rather that they will match
-something unexpected. I wrote this web service to help identify these issues by
-showing a list of random strings that would match a given regex.""" )
-                 , github "ServerHere"
-                     (ExternalUrl "https://github.com/lorcanmcdonald/ServerHere")
-                     Nothing
-                     (Markdown.toHtml """
-`ServerHere` is a script to run a simple web server based in a particular
-directory. It's convenient for working with small Javascript modules.
-I've only recently published it on Github, but it actually predates
-[`http-server`](https://github.com/indexzero/http-server).
-
-No, I've never heard of [`python -m
-SimpleHTTPServer`](https://docs.python.org/2/library/simplehttpserver.html),
-why do you ask?""")
                  ]
 
 break : Html
@@ -120,11 +113,10 @@ break = div [ style [("page-break-after", "always")]] []
 work : Html
 work = section []
                [ h2 [ id "WorkExperience" ] [ text "Work Experience" ]
+               , melosity
                , serviceFrame
-               , break
                , ibm
                , deecal
-               , break
                , tradesports
                ]
 
@@ -140,6 +132,9 @@ dcu = position "BSc. Computer Applications (Software Engineering)"
                (Markdown.toHtml """
 Computer Applications is a four years honours degree covering practical aspects
 of software engineering and theoretical topics in computer science.
+
+My final year project was an implementation of a neural network for use in
+classifying email as spam or non-spam.
 
 I took a number of different modules Modules including:
 - Software Engineering
@@ -172,17 +167,52 @@ Open Source project.
 
 While with IBM I had the opportunity to contribute a chapter to the [XPages
 Extension Library](http://www.amazon.com/XPages-Extension-Library-Step-Step/dp/0132901811) book
-and present to large audiences[¹](#ibm-footnote1) (including Lotusphere 2012). I also released a
+and present to large audiences[³](#ibm-footnote1) (including Lotusphere 2012). I also released a
 small open source library, [xspUnit](http://extlib.openntf.org/main.nsf/project.xsp?r=project/XSPUnit/releases/3A1C5BE6C1A246CA862575A10034822C),
 to allow unit testing of Server Side Javascript libraries in XPages.
 
-<a name="ibm-footnote1"></a>¹ Topics included: Mobile Development, graphics programming using Quartz Mac OS X and the Vim text editor
+<a name="ibm-footnote1"></a>³ Topics included: Mobile Development, graphics programming using Quartz Mac OS X and the Vim text editor
+""")
+
+melosity : Html
+melosity = position "Chief Technical Officer"
+                        (Company "Melosity" (toUrl "http://melosity.com"))
+                        (TimeSpan "Nov 2015 → Present")
+               (Markdown.toHtml """
+At Melosity we're changing the way that musicians
+collaborate together. We have built a way to record and arrange a music project
+directly in the browser between multiple contributers, in real time[¹](#melosity-footnote1).
+
+As the CTO of an early stage start up, my responsibilites were split between
+between architecting the product and infrastructure of the product and scaling
+up the team. We launched to the public in September 2016.
+
+Melosity is primarily built using [React](https://facebook.github.io/react/),
+[Haskell](https://www.haskell.org) and [Postgres](https://www.postgresql.org)
+stack. We use the microservices design pattern to for continuous deployment and
+decoupling of unrelated processes. [Docker](https://www.docker.com),
+[Elastic Beanstalk](https://aws.amazon.com/elasticbeanstalk/details/) and
+[Terraform](https://www.terraform.io) allow us
+[Continously Deliver](https://en.wikipedia.org/wiki/Continuous_delivery) as
+tasks are reviewed and completed.
+
+As important as the product was the team. I instituted a agile development
+process based on [Kanban](https://en.wikipedia.org/wiki/Kanban_(development)).
+We hold regular retrospective meetings with all stakeholders to improve our
+throughput and to help non-technical team members to understand and contribute
+to the software development process. In addition I hold regular one-to-one
+meetings with the engineering team to make sure we are meeting their objectives
+with respect to their professional practice.
+
+<a name="melosity-footnote1"></a>¹ [Firm Real Time](https://en.wikipedia.org/wiki/Real-time_computing#Criteria_for_real-time_computing).
+We need to process each frame of audio within approximately 6ms to ensure that
+a user's recording doesn't glitch.
 """)
 
 serviceFrame : Html
 serviceFrame = position "Principal Engineer"
                         (Company "ServiceFrame" (toUrl "http://serviceframe.com"))
-                        (TimeSpan "May 2012 → Present")
+                        (TimeSpan "May 2012 → Nov 2015")
                (Markdown.toHtml """
 ServiceFrame is a governance platform for outsourced relationships, typically
 (but not limited to) the Telecoms Managed Services sector. It allows executive
@@ -198,7 +228,7 @@ continuously analysing the process to identify where we can make the process
 more predictable and efficient.
 
 It is comprised of a Single Page Javascript client application using an in house
-CoffeeScript framework[¹](#sf-footnote1), a .Net 4.5 C# application serving a JSON
+CoffeeScript framework, a .Net 4.5 C# application serving a JSON
 REST API and a Data Integration pipeline using a number of third party
 ETL[²](#sf-footnote2) tools. These services were backed by both Relational (MySQL,
 SQL Server and Postgres) and NoSQL (MongoDB) databases. A number of subsystems
@@ -221,16 +251,9 @@ the existing infrastructure from an hosted co-location solution to AWS and
 growing the infrastructure once we cut over to provide more reliable and safe
 deployments. I used tools such as [Vagrant](http://vagrantup.com),
 [Puppet](https://puppetlabs.com/puppet/what-is-puppet) and
-[Docker](https://www.docker.com/whatisdocker) to produce an [Immutable
+Docker to produce an [Immutable
 Infrastructure](http://martinfowler.com/bliki/ImmutableServer.html#footnote-coin)
 style deployment.
-
-<a name="sf-footnote1"></a>¹ There were a couple of reasons that we had to roll
-our own framework here. At the time that we started the project, frameworks
-like EmberJS, Knockout or React were in their infancy. Also, as we have users
-based in a number of African countries, we had a number of design constraints
-around performance. Both bandwidth and latency, and the low spec machines that
-might be used by the users.
 
 <a name="sf-footnote2"></a>² [Extract, Transform, Load](https://en.wikipedia.org/wiki/Extract,_transform,_load)
 """)
@@ -239,7 +262,7 @@ deecal : Html
 deecal = position "Senior Full Stack Developer"
                   (Company "Deecal International (later acquired by FirstData)" (toUrl "http://www.deecal.ie"))
                   (TimeSpan "Nov 2006 → Sept 2008")
-               (Markdown.toHtml """"
+               (Markdown.toHtml """
 At Deecal I worked on the main product d.cal. d.cal was an suite of
 applications for managing corporate credit cards. It allowed you to monitor
 real time transactions, handle expense claims and other common functions on
@@ -252,8 +275,8 @@ and later Tomcat) which then communicated with a Oracle 10g database.
 The nature of the intercommunication between these individual iframe window was
 causing a lot of trouble for the team and they required someone with expert
 knowledge of front end development. I was taken on as a full stack developer
-with special responsibility for improving knowledge an process in the front end
-code.
+with special responsibility for improving knowledge and processes in the front
+end code.
 
 This allowed me to introduce the team to the (at the time still reasonably new)
 concepts of [AJAX](https://en.wikipedia.org/wiki/Ajax_(programming)),
@@ -272,16 +295,16 @@ tradesports = position "Software Developer"
 Trade Exchange Network ran a number of prediction market products open to
 public customers and in-house for a number of private clients. The consumer
 sites included intrade.com, which catered primarily to financial and public
-policy contracts and tradesports.com which modelled sports betting as a futures
+policy contracts and tradesports.com, which modelled sports betting as a futures
 market.
 
-This was my first position after completing my studies and I was lucky to gain
-a large amount of responsibility for large parts of the system quite early on
-as I showed an aptitude for web development and UNIX systems administration.
+This was my first position after completing my studies and I quickly gained
+responsibility for large parts of the system as I showed an aptitude for web
+development and UNIX systems administration.
 
 I was personally responsible for the frontend code (i.e. Java Server Pages,
 Javascript, HTML and CSS) for all the consumer properties. During my time there
-I wrote an AJAX trading interface to show price updates in real time and allow
+I wrote an AJAX trading interface to show price updates in real time[⁴](tradesports-footnote1) and allow
 users to buy and sell contracts. Speed and correctness was particularly
 important as this would be used to trade real money during fast moving sporting
 and political events.
@@ -290,6 +313,8 @@ I was responsible for converting the sites from tables based designs to CSS. Thi
 allowed us to swiftly produce high quality reskined sites for internal
 corporate markets, typically within hours of a request coming in, this would
 have taken weeks of work previously and was prone to mistakes.
+
+<a name="tradesports-footnote1"></a>⁴ Soft real time.
 """)
 
 github : String -> Url -> Maybe Url -> Html -> Html
